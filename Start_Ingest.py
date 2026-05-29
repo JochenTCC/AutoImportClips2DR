8b25c65d69261d382dcdbe4f0119e2b889cd623f
JochenTCC <164034604+JochenTCC@users.noreply.github.com>
2026-05-29 15:19:33 +0200

    Create Start_Ingest.py

---
+++ Start_Ingest.py
@@ -0,0 +1,6 @@
+import sys
+# Pfad zu deinem neuen Speicherort, wo die Dateien wirklich liegen
+sys.path.append(r"C:\Users\joche\Documents\GitHub\AutoImportClips2DR\Scripts")
+
+import main_gui
+# Das löst die GUI aus
